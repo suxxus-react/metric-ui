a2f6218720023a9835aa57384312ac195fadb9cf
d <xx@x.com>
2022-12-27 14:49:39 +0100

    Create metric-ui layout container

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,18 @@
-import { useState } from "react";
+// import { useState } from "react";
 import Metric from "./components/Metric-ui";
 
+// <Metric />
 function App() {
   return (
-    <div className="m-5">
+    <div className="grid gap-4 grid-cols-1 md:grid-cols-2 lg:grid-cols-3 max-w-xs sm:max-w-xs  md:max-w-screen-sm lg:max-w-screen-lg my-4 mx-auto">
+      <Metric />
+      <Metric />
+      <Metric />
+      <Metric />
+      <Metric />
+      <Metric />
+      <Metric />
+      <Metric />
       <Metric />
     </div>
   );
