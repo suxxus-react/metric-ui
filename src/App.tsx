db77c6d74c5ac0cefa2c07732b8b70c80b12e69b
d <xx@x.com>
2022-12-27 15:28:13 +0100

    Remove not needed class from Layout

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import Metric from "./components/Metric-ui";
 // <Metric />
 function App() {
   return (
-    <div className="grid gap-4 grid-cols-1 md:grid-cols-2 lg:grid-cols-3 max-w-xs sm:max-w-xs  md:max-w-screen-sm lg:max-w-screen-lg my-4 mx-auto">
+    <div className="grid gap-4 grid-cols-1 md:grid-cols-2 lg:grid-cols-3 max-w-xs md:max-w-screen-sm lg:max-w-screen-lg my-4 mx-auto">
       <Metric />
       <Metric />
       <Metric />
