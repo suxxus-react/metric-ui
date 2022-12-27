7be491da874f30342323b7ae2b7e509d6700a55e
d <xx@x.com>
2022-12-27 15:52:09 +0100

    Create header App

--- src/App.tsx
+++ src/App.tsx
@@ -4,17 +4,28 @@ import Metric from "./components/Metric-ui";
 // <Metric />
 function App() {
   return (
-    <div className="grid gap-4 grid-cols-1 md:grid-cols-2 lg:grid-cols-3 max-w-xs md:max-w-screen-sm lg:max-w-screen-lg my-4 mx-auto">
-      <Metric />
-      <Metric />
-      <Metric />
-      <Metric />
-      <Metric />
-      <Metric />
-      <Metric />
-      <Metric />
-      <Metric />
-    </div>
+    <>
+      <header>
+        <div>logo </div>
+        <button>new metric</button>
+        <div>
+          <span>user name</span>
+          <i className="fa fa-user fa-2x"></i>
+        </div>
+        <button>edit</button>
+      </header>
+      <div className="grid gap-4 grid-cols-1 md:grid-cols-2 lg:grid-cols-3 max-w-xs md:max-w-screen-sm lg:max-w-screen-lg my-4 mx-auto">
+        <Metric />
+        <Metric />
+        <Metric />
+        <Metric />
+        <Metric />
+        <Metric />
+        <Metric />
+        <Metric />
+        <Metric />
+      </div>
+    </>
   );
 }
 
