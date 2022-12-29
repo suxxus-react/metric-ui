e8dcd01d08f6f4cc377f20f62aae035a09abea1c
d <xx@x.com>
2022-12-29 19:55:38 +0100

    Update styles

--- src/components/Page-edit-metrics.tsx
+++ src/components/Page-edit-metrics.tsx
@@ -2,11 +2,11 @@ import LoggedHeader from "./Logged-header";
 import Metric from "./Metric-editor";
 export default function PageEditMetrics(): JSX.Element {
   return (
-    <div className="max-w-xs md:max-w-screen-sm lg:max-w-screen-lg mx-auto">
-      <section>
+    <div className="h-full overflow-hidden max-w-xs md:max-w-screen-sm lg:max-w-screen-lg mx-auto">
+      <section className="my-5">
         <LoggedHeader />
       </section>
-      <section>
+      <section className="h-full overflow-scroll">
         <div className="grid gap-4 grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
           <Metric />
           <Metric />
