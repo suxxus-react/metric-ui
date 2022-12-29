f5830f9ff87b301f690fc30dade5c71eae98078d
d <xx@x.com>
2022-12-29 18:15:44 +0100

    Update component styles

--- src/components/Login-with-social-account.tsx
+++ src/components/Login-with-social-account.tsx
@@ -1,9 +1,10 @@
 //
 export default function LoginWithAccount(): JSX.Element {
-  const Button = "flex justify-around w-52 bg-gray-300 font-medium p-3 rounded";
+  const Button =
+    "flex justify-center w-full h-14 bg-gray-300 font-medium p-3 rounded-lg text-black md:w-60";
   return (
     <a href="#" className={Button}>
-      <i className="fa fa-github-alt fa-2x" aria-hidden="true"></i>
+      <i className="fa fa-github-alt fa-2x mr-8" aria-hidden="true"></i>
       <span className="font-extrabold">Login with Github</span>
     </a>
   );
