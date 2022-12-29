07519c1e6dcec2d18a1df358ed32d507dcd776f7
d <xx@x.com>
2022-12-29 18:15:55 +0100

    Update component styles

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,6 @@
 export default function Header() {
   return (
-    <header className="mb-6 flex justify-between">
+    <header className="flex justify-between">
       <h1 className="metrics__logo">fun-metrics</h1>
       <button>
         <i className="fa fa-sun-o"></i>
