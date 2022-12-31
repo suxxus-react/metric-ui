6a3206b8752e3d7ffe9b6912758190d9cac03b7d
d <xx@x.com>
2022-12-31 14:40:02 +0100

    Create App types
    globally

---
+++ src/metricfun.types.ts
@@ -0,0 +1,70 @@
+type DeleteMetric = {
+  type: "DeleteMetric";
+  id: string;
+};
+type ShowWarning = {
+  type: "ToggleShowWarning";
+  id: string;
+};
+
+type EditMetricName = {
+  type: "EditMetricName";
+  id: string;
+};
+
+type UpdateMetricName = {
+  type: "UpdateMetricName";
+  id: string;
+  value: string;
+};
+type RenameMetric = {
+  type: "RenameMetric";
+  id: string;
+};
+
+type ToggleDarkMode = {
+  type: "ToggleDarkMode";
+};
+
+type None = {
+  type: "None";
+};
+export type DispatchMsg = (msg: Msg) => Msg;
+
+export type Msg =
+  | ToggleDarkMode
+  | DeleteMetric
+  | ShowWarning
+  | EditMetricName
+  | UpdateMetricName
+  | RenameMetric
+  | None;
+type Metadata = {
+  update: string;
+  limit: string;
+  resolution: string;
+};
+
+export type MetricUi = {
+  id: string;
+  name: string;
+  isMetricNameEditable: boolean;
+  isDeleted: boolean;
+  isEditable: boolean;
+  showWarning: boolean;
+  metadata: Metadata;
+  handleClick: DispatchMsg;
+  handleOnChange: DispatchMsg;
+};
+
+export type State = {
+  isDark: boolean;
+  isLogged: boolean;
+  userName: string;
+  isEditable: boolean;
+  //
+  handleClick: DispatchMsg;
+  toggleEditable: () => void;
+  createNewMetric: () => void;
+  loginWithAccount: () => void;
+};
