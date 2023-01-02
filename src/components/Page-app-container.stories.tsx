075893de45cf9032ad9438c4ad9dcf2be31b8dbb
d <xx@x.com>
2023-01-02 01:48:40 +0100

    Add metricUi properties to  Page-app-container

--- src/components/Page-app-container.stories.tsx
+++ src/components/Page-app-container.stories.tsx
@@ -1,6 +1,7 @@
 import { ComponentStory, ComponentMeta } from "@storybook/react";
 import MainContainer from "./Page-app-container";
 import { IProps } from "../metricfun.types";
+import { MetricUi } from "../metricfun.types";
 export default {
   title: "Metric-fun/Main-container",
   component: MainContainer,
@@ -14,14 +15,38 @@ const Template: ComponentStory<typeof MainContainer> = (args) => (
   <MainContainer {...args} />
 );
 
+const metricUiProps: MetricUi = {
+  id: "01",
+  name: "metric name",
+  isMetricNameEditable: false,
+  isEditable: true,
+  isSavingChanges: false,
+  showUpdateMetricChanges: false,
+  showWarning: false,
+  chartTypeSelected: "None",
+  metadata: {
+    resolution: "monthly",
+    update: "update ...",
+    limit: "limit x",
+  },
+  handleOnChange: () => ({ type: "None" }),
+  handleClick: () => ({ type: "None" }),
+};
+
 const props: Omit<IProps, "handleClick" | "handleOnChange"> = {
   isDark: false,
   isLogged: false,
   isEditable: false,
   userName: "",
+  metrics: [],
 };
 
 export const Login = Template.bind({});
-Login.args = { ...props, isLogged: false };
+Login.args = { ...props };
+
 export const UserLogged = Template.bind({});
-UserLogged.args = { ...props, isLogged: true };
+UserLogged.args = {
+  ...props,
+  isLogged: true,
+  metrics: [...Array(3)].fill({ ...metricUiProps }),
+};
