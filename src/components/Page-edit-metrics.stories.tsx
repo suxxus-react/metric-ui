bff18c66c45b2ff5bdb3ad07219c2f9e980c58fe
d <xx@x.com>
2023-01-02 01:47:39 +0100

    Should map MetricUi[]

--- src/components/Page-edit-metrics.stories.tsx
+++ src/components/Page-edit-metrics.stories.tsx
@@ -1,14 +1,53 @@
 import React from "react";
 import { ComponentStory, ComponentMeta } from "@storybook/react";
+import { IProps } from "../metricfun.types";
 import PageEditMetrics from "./Page-edit-metrics";
+import { MetricUi } from "../metricfun.types";
 
 export default {
   title: "Metric-fun/Page-edit-metrics",
   component: PageEditMetrics,
+  argTypes: {
+    handleClick: { action: "msg" },
+    handleOnChange: { action: "msg" },
+  },
 } as ComponentMeta<typeof PageEditMetrics>;
-//
-const Template: ComponentStory<typeof PageEditMetrics> = () => (
-  <PageEditMetrics />
+
+const metricUiProps: MetricUi = {
+  id: "01",
+  name: "metric name",
+  isMetricNameEditable: false,
+  isEditable: true,
+  isSavingChanges: false,
+  showUpdateMetricChanges: false,
+  showWarning: false,
+  chartTypeSelected: "None",
+  metadata: {
+    resolution: "monthly",
+    update: "update ...",
+    limit: "limit x",
+  },
+  handleOnChange: () => ({ type: "None" }),
+  handleClick: () => ({ type: "None" }),
+};
+
+const props: Omit<IProps, "handleClick" | "handleOnChange"> = {
+  isDark: false,
+  isLogged: false,
+  isEditable: false,
+  userName: "",
+  metrics: [],
+};
+
+const Template: ComponentStory<typeof PageEditMetrics> = (args) => (
+  <PageEditMetrics {...args} />
 );
 
-export const EditMetric = Template.bind({});
+export const WithOutMetricData = Template.bind({});
+WithOutMetricData.args = { ...props };
+
+export const WithStoredMetricData = Template.bind({});
+WithStoredMetricData.args = {
+  ...props,
+  metrics: [...Array(6)].fill({ ...metricUiProps }),
+};
--- src/components/Page-edit-metrics.tsx
+++ src/components/Page-edit-metrics.tsx
@@ -1,26 +1,20 @@
 import LoggedHeader from "./Logged-header";
 import Metric from "./Metric-editor";
-import { State } from "../metricfun.types";
-export default function PageEditMetrics(state: State): JSX.Element {
+import { IProps } from "../metricfun.types";
+export default function PageEditMetrics(props: IProps): JSX.Element {
+  const { metrics } = props;
   return (
     <div className="h-full overflow-hidden max-w-xs md:max-w-screen-sm lg:max-w-screen-lg mx-auto">
       <section className="my-5">
-        <LoggedHeader {...state} />
+        <LoggedHeader {...props} />
       </section>
       <section className="h-full overflow-scroll">
-        <div className="grid gap-4 grid-cols-1 md:grid-cols-2 lg:grid-cols-3"></div>
+        <div className="grid gap-4 grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
+          {metrics.map((props) => {
+            return <Metric {...props} />;
+          })}
+        </div>
       </section>
     </div>
   );
 }
-
-// <Metric />
-// <Metric />
-// <Metric />
-// <Metric />
-// <Metric />
-// <Metric />
-// <Metric />
-// <Metric />
-// <Metric />
-// <Metric />
