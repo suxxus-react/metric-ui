26d23a18f9fff63f8f325d17b79c28669b4554b0
d <xx@x.com>
2023-01-11 13:37:13 +0100

    Refactor
    to slim App file

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import { useState, useEffect } from "react";
-import { nanoid } from "nanoid";
 import axios from "axios";
 import MainContainer from "./components/Page-app-container";
 import { userDataDecoder, newMetricDataDecoder } from "./jsonDataDecoders";
@@ -8,12 +7,12 @@ import {
   Msg,
   IProps,
   ChartTypeSelected,
-  PieChartDataSets,
-  LineChartDataSet,
-  MetricData,
   IMetricUi,
-  MetricDataSet,
 } from "./metricfun.types";
+import {
+  getDefaultMetricUiData,
+  updateMetricUiData,
+} from "./metricDataHelpers";
 
 type MetricUpdatedData = {
   id: string;
@@ -21,177 +20,6 @@ type MetricUpdatedData = {
   chartType: ChartTypeSelected;
 };
 type MetricId = { id: string };
-
-function getChartTypeSelected(chartType: string): ChartTypeSelected {
-  switch (chartType.toUpperCase()) {
-    case "PIE":
-      return "Pie";
-    case "AREA":
-      return "Area";
-    case "LINE":
-      return "Line";
-    default:
-      return "None";
-  }
-}
-
-function getPieChartData({
-  label = "",
-  data,
-}: MetricDataSet): PieChartDataSets {
-  //
-  const backgroundColor = [
-    "rgba(255, 99, 132, 0.2)",
-    "rgba(54, 162, 235, 0.2)",
-    "rgba(255, 206, 86, 0.2)",
-    "rgba(75, 192, 192, 0.2)",
-    "rgba(153, 102, 255, 0.2)",
-    "rgba(255, 159, 64, 0.2)",
-  ];
-
-  const borderColor = [
-    "rgba(255, 99, 132, 1)",
-    "rgba(54, 162, 235, 1)",
-    "rgba(255, 206, 86, 1)",
-    "rgba(75, 192, 192, 1)",
-    "rgba(153, 102, 255, 1)",
-    "rgba(255, 159, 64, 1)",
-  ];
-
-  return {
-    label,
-    data,
-    backgroundColor,
-    borderColor,
-    borderWidth: 1,
-  };
-}
-
-function getLineChartData(fill: boolean) {
-  return (
-    { label = "", data }: MetricDataSet,
-    index: number
-  ): LineChartDataSet => {
-    const styles = [
-      {
-        // blue
-        borderColor: "rgb(53, 162, 235)",
-        backgroundColor: "rgba(53, 162, 235, 0.5)",
-      },
-      {
-        // red
-        borderColor: "rgb(255, 99, 132)",
-        backgroundColor: "rgba(255, 99, 132, 0.5)",
-      },
-      {
-        // green
-        borderColor: "rgb(28, 121, 28)",
-        backgroundColor: "rgb(28, 121, 28, 0.5)",
-      },
-    ];
-
-    return {
-      fill,
-      label,
-      data,
-      ...(styles[index] || {
-        borderColor: "rgb(204, 204, 204)",
-        backgroundColor: "rgb(204, 204, 204, 0.5)",
-      }),
-    };
-  };
-}
-
-function updateMetricUiData(metricData: MetricData): IMetricUi {
-  const labels = metricData.chartData.labels || [];
-
-  const metadata = metricData.metadata || {
-    resolution: "",
-    update: "",
-    limit: "",
-  };
-
-  const errorTypes = {
-    nameLength: false,
-    nameEquals: false,
-    noChartSelected: false,
-  };
-
-  const defaultMetricData: IMetricUi = {
-    id: metricData.id,
-    previousName: metricData.name,
-    name: metricData.name,
-    isNewMetric: false,
-    isMetricNameEditable: false,
-    isEditable: false,
-    isSavingChanges: false,
-    requestMetricDeletion: false,
-    showMetricSaveCancelCtrls: false,
-    isValid: false,
-    errorTypes,
-    originalChartTypeSelected: getChartTypeSelected(metricData.chartType),
-    chartTypeSelected: getChartTypeSelected(metricData.chartType),
-    chartsData: {
-      pie: {
-        datasets: metricData.chartData.datasets.map(getPieChartData),
-        labels,
-      },
-      area: {
-        datasets: metricData.chartData.datasets.map(getLineChartData(true)),
-        labels,
-      },
-      line: {
-        datasets: metricData.chartData.datasets.map(getLineChartData(false)),
-        labels,
-      },
-    },
-    metadata,
-  };
-
-  return defaultMetricData;
-}
-
-// metricUi transform data Helpers
-// ===============================
-
-// default metricUI data
-function getDefaultMetricUiData(): IMetricUi {
-  const datasets = { datasets: [], labels: [] };
-
-  const chartsData = {
-    pie: datasets,
-    area: datasets,
-    line: datasets,
-  };
-
-  const errorTypes = {
-    nameLength: false,
-    nameEquals: false,
-    noChartSelected: false,
-  };
-
-  return {
-    id: nanoid(),
-    previousName: "",
-    name: "",
-    isNewMetric: true,
-    isMetricNameEditable: true,
-    isEditable: true,
-    isSavingChanges: false,
-    requestMetricDeletion: false,
-    showMetricSaveCancelCtrls: true,
-    isValid: false,
-    originalChartTypeSelected: "None",
-    chartTypeSelected: "None",
-    errorTypes,
-    chartsData,
-    metadata: {
-      resolution: "",
-      update: "",
-      limit: "",
-    },
-  };
-}
 
 function updateStateMetricList(
   msg: Msg,
@@ -370,7 +198,6 @@ function App() {
 
   useEffect(() => {
     let updatedState: IState = state;
-
     if (import.meta.env.DEV) {
       console.log("msg ", msg);
     }
