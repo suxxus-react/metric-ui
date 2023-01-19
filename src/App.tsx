4c4166ad408d62df63985da41fa9e56dc8b03c69
d <xx@x.com>
2023-01-19 11:41:07 +0100

    WIP, refactor

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,23 @@
 import { useEffect, useReducer } from "react";
-import { useNavigate } from "react-router-dom";
-import axios from "axios";
+// import { useNavigate } from "react-router-dom";
+// import axios from "axios";
 import MainContainer from "./components/Page-app-container";
-import { userDataDecoder, newMetricDataDecoder } from "./jsonDataDecoders";
-import { IState, IProps } from "./metricfun.types";
+import {
+  userDataDecoder,
+  deleteMetricDecoder,
+  newMetricDataDecoder,
+} from "./jsonDataDecoders";
+
+import { useGetApi, useDeleteApi, useUpdateApi } from "./apiCall";
+
+import { IState, IProps, Status } from "./metricfun.types";
 import { updateMetricUiData } from "./metricDataHelpers";
 import { stateReducer } from "./reducers";
 
 const INITIAL_STATE: IState = {
   id: 0,
   isDark: false,
-  isLogged: false,
+  isLogged: true,
   userName: "",
   isEditable: false,
   deleteMetric: { id: "" },
@@ -29,29 +36,12 @@ const INITIAL_STATE: IState = {
 
 function App() {
   const [state, dispatch] = useReducer(stateReducer, INITIAL_STATE);
-  const navigate = useNavigate();
 
-  useEffect(() => {
-    if (!state.isLogged) {
-      async function fetchMetricsData() {
-        try {
-          const response = await axios.get("/data.json");
-          const { data }: { data: unknown } = response;
+  const [{ data, error }, doFetch] = useGetApi();
+  const [{ deleteData, deleteErr }, doDelete] = useDeleteApi();
+  const [{ updateData, updateErr }, updateBody, doPut] = useUpdateApi();
 
-          dispatch({
-            type: "UpdateMetricList",
-            value: userDataDecoder(data).metrics.map(updateMetricUiData),
-          });
-          navigate("/welcome"); // TODO remove whe github login is finished
-
-          /* eslint-disable-next-line @typescript-eslint/no-explicit-any */
-        } catch (err: any) {
-          console.error(err.message);
-        }
-      }
-      fetchMetricsData();
-    }
-  }, [state.isLogged, navigate]);
+  // const navigate = useNavigate();
 
   useEffect(() => {
     if (state.isDark) {
@@ -62,99 +52,120 @@ function App() {
   }, [state.isDark]);
 
   useEffect(() => {
-    if (state.deleteMetric.id) {
-      async function deleteUserMetric() {
-        try {
-          const response = await axios.delete("http://dummyjson.com/http/200", {
-            data: { id: state.deleteMetric.id },
-          });
-
-          const { data }: { data: { status?: string } } = response;
-
-          if (data.status === "200") {
-            dispatch({
-              type: "MetricDeleted",
-              id: state.deleteMetric.id,
-            });
-          }
-          /* eslint-disable-next-line @typescript-eslint/no-explicit-any */
-        } catch (err: any) {
-          console.error(err.message);
-        }
+    if (state.isLogged) {
+      if (error) {
+        console.error(error);
       }
-      deleteUserMetric();
+
+      if (data) {
+        dispatch({
+          type: "UpdateMetricList",
+          value: userDataDecoder(data).metrics.map(updateMetricUiData),
+        });
+      } else {
+        doFetch("/data.json");
+      }
     }
-  }, [state.deleteMetric.id]);
+  }, [state.isLogged, data, error, doFetch]);
 
   useEffect(() => {
-    if (state.updateMetricChanges.id) {
-      async function updateMetric() {
-        try {
-          const response = await axios.put("https://dummyjson.com/http/200", {
-            name: state.updateMetricChanges.name,
-            id: state.updateMetricChanges.id,
-            chartType: state.updateMetricChanges.chartType,
-          });
-
-          const { data }: { data: { status?: string } } = response;
-
-          if (data.status === "200") {
-            dispatch({
-              type: "MetricUpdated",
-              id: state.updateMetricChanges.id,
-            });
-          } else {
-            console.warn("no matches", data);
-          }
-
-          /* eslint-disable-next-line @typescript-eslint/no-explicit-any */
-        } catch (err: any) {
-          console.error(err.message || "");
-        }
+    if (state.deleteMetric.id) {
+      if (deleteErr) {
+        console.error(deleteErr);
       }
 
-      updateMetric();
+      if (deleteData) {
+        const decoded = deleteMetricDecoder(deleteData);
+        if (decoded.status === Status.Ok) {
+          dispatch({
+            type: "MetricDeleted",
+            id: state.deleteMetric.id,
+          });
+        } else {
+          console.warn(decoded.status);
+        }
+      } else {
+        doDelete("http://dummyjson.com/http/200");
+      }
+    }
+  }, [state.deleteMetric.id, deleteErr, deleteData, doDelete]);
+  //
+  useEffect(() => {
+    if (state.updateMetricChanges.id) {
+      if (updateError) {
+        console.error(updateErr);
+      }
+
+      if (updateData) {
+        // const decoded =
+      }
+      // async function updateMetric() {
+      //   try {
+      //     const response = await axios.put("https://dummyjson.com/http/200", {
+      //       name: state.updateMetricChanges.name,
+      //       id: state.updateMetricChanges.id,
+      //       chartType: state.updateMetricChanges.chartType,
+      //     });
+      //
+      //     const { data }: { data: { status?: string } } = response;
+      //
+      //     if (data.status === "200") {
+      //       dispatch({
+      //         type: "MetricUpdated",
+      //         id: state.updateMetricChanges.id,
+      //       });
+      //     } else {
+      //       console.warn("no matches", data);
+      //     }
+      //
+      //     /* eslint-disable-next-line @typescript-eslint/no-explicit-any */
+      //   } catch (err: any) {
+      //     console.error(err.message || "");
+      //   }
+      // }
+      //
+      // updateMetric();
     }
   }, [
     state.updateMetricChanges.id,
     state.updateMetricChanges.chartType,
     state.updateMetricChanges.name,
   ]);
-
-  useEffect(() => {
-    if (state.saveNewMetricChanges.id) {
-      async function saveMetric() {
-        try {
-          const response = await axios.post("/newMetric.json", {
-            name: state.saveNewMetricChanges.name,
-            id: state.saveNewMetricChanges.id,
-            chartType: state.saveNewMetricChanges.chartType,
-          });
-          const { data }: { data: { status?: string } } = response;
-
-          dispatch({
-            type: "NewMetricUpdated",
-            id: state.saveNewMetricChanges.id,
-            value: {
-              ...newMetricDataDecoder(data),
-              name: state.saveNewMetricChanges.name,
-              id: state.saveNewMetricChanges.id,
-            },
-          });
-
-          /* eslint-disable-next-line @typescript-eslint/no-explicit-any */
-        } catch (err: any) {
-          console.error(err.message || "");
-        }
-      }
-
-      saveMetric();
-    }
-  }, [
-    state.saveNewMetricChanges.id,
-    state.saveNewMetricChanges.name,
-    state.saveNewMetricChanges.chartType,
-  ]);
+  //
+  // useEffect(() => {
+  //   if (state.saveNewMetricChanges.id) {
+  //     async function saveMetric() {
+  //       try {
+  //         const response = await axios.post("/newMetric.json", {
+  //           name: state.saveNewMetricChanges.name,
+  //           id: state.saveNewMetricChanges.id,
+  //           chartType: state.saveNewMetricChanges.chartType,
+  //         });
+  //         const { data }: { data: { status?: string } } = response;
+  //
+  //         dispatch({
+  //           type: "NewMetricUpdated",
+  //           id: state.saveNewMetricChanges.id,
+  //           value: {
+  //             ...newMetricDataDecoder(data),
+  //             name: state.saveNewMetricChanges.name,
+  //             id: state.saveNewMetricChanges.id,
+  //           },
+  //         });
+  //
+  //         /* eslint-disable-next-line @typescript-eslint/no-explicit-any */
+  //       } catch (err: any) {
+  //         console.error(err.message || "");
+  //       }
+  //     }
+  //
+  //     saveMetric();
+  //   }
+  // }, [
+  //   state.saveNewMetricChanges.id,
+  //   state.saveNewMetricChanges.name,
+  //   state.saveNewMetricChanges.chartType,
+  // ]);
 
   const props: IProps = {
     ...state,
