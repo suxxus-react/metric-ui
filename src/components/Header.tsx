c60f7cf25392d07d340c335c6ee11d53e47322a6
d <xx@x.com>
2023-01-22 20:29:33 +0100

    Create conditional to show Metric Logo, for dark mode

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,9 +2,14 @@ import { IProps } from "../metricfun.types";
 export default function Header({ isDark, dispatchMsg }: IProps): JSX.Element {
   const sun = <i className="fa fa-sun-o"></i>;
   const moon = <i className="fa fa-moon-o"></i>;
+
   return (
     <header className="flex justify-between">
-      <div className="metric-fun-rgb bg-no-repeat w-24 h-20"></div>
+      <div
+        className={`metric-fun-rgb${
+          isDark ? "-light" : ""
+        } bg-no-repeat w-24 h-20`}
+      ></div>
       <button
         className="button-default"
         onClick={() => {
